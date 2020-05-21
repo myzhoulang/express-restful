08c603f5813c8881a4cbae7eba230a02fe7bb23b
周浪 <lang.zhou@fulintechfin.com>
2020-05-21 18:36:39 +0800

    test sentry

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import * as Sentry from 'src/util/sentry';
+
 export const getApp = () => {
   const app: Application = express();
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
 
   app.get('/api/v1/test', (_: Request, res: Response) => {
-    throw new Error('hello123');
+    throw new Error('76489');
     res.json({ ok: true });
   });
 
--- src/util/sentry.ts
+++ src/util/sentry.ts
@@ -2,6 +2,17 @@ import * as express from 'express';
 import * as Sentry from '@sentry/node';
 import * as Integrations from '@sentry/integrations';
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface Global {
+      __rootdir__: string;
+    }
+  }
+}
+
+global.__rootdir__ = __dirname || process.cwd();
+
 const request = (app: express.Application) => {
   // 只有在非开发环境下才启用 sentry
   const { SENTRY_DSN, NODE_ENV, SENTRY_RELEASE } = process.env;
@@ -12,7 +23,11 @@ const request = (app: express.Application) => {
       dsn: SENTRY_DSN,
       environment: NODE_ENV,
       release: SENTRY_RELEASE,
-      integrations: [new Integrations.RewriteFrames()],
+      integrations: [
+        new Integrations.RewriteFrames({
+          root: global.__rootdir__,
+        }),
+      ],
     });
     app.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
   }
