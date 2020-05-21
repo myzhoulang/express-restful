86c8aa6c5fe69422b761a2b38830409923f8f74d
周浪 <lang.zhou@fulintechfin.com>
2020-05-21 18:15:42 +0800

    add Sentry

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
+import * as Sentry from 'src/util/sentry';
 export const getApp = () => {
   const app: Application = express();
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
 
   app.get('/api/v1/test', (_: Request, res: Response) => {
+    throw new Error('hello123');
     res.json({ ok: true });
   });
+
+  Sentry.error(app);
 
   app.use((error: HttpError, req: Request, res: Response, next: NextFunction) => {
     console.error(error);
--- src/util/sentry.ts
+++ src/util/sentry.ts
@@ -1,21 +1,24 @@
 import * as express from 'express';
-import Sentry from '@sentry/node';
-import { Application } from 'express';
+import * as Sentry from '@sentry/node';
+import * as Integrations from '@sentry/integrations';
 
-const request = (app: Application) => {
+const request = (app: express.Application) => {
   // 只有在非开发环境下才启用 sentry
   const { SENTRY_DSN, NODE_ENV, SENTRY_RELEASE } = process.env;
+  console.log(NODE_ENV);
   if (NODE_ENV !== 'development') {
+    console.log(Sentry);
     Sentry.init({
       dsn: SENTRY_DSN,
       environment: NODE_ENV,
       release: SENTRY_RELEASE,
+      integrations: [new Integrations.RewriteFrames()],
     });
     app.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
   }
 };
 
-const error = (app: Application) => {
+const error = (app: express.Application) => {
   if (process.env.NODE_ENV !== 'development') {
     app.use(Sentry.Handlers.errorHandler() as express.ErrorRequestHandler);
   }
