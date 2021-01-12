d48656ece2425461174e4b04143b8a65dd0d5562
zhoulang <zhoulang@hejunchuxing.com>
2021-01-12 20:48:20 +0800

    🐞fix(middleware/log.ts): 修改字段request_methods
    修改字段 request_methods 为 request_method

--- src/middleware/log.ts
+++ src/middleware/log.ts
@@ -2,12 +2,13 @@ import { Request, Response, NextFunction } from 'express'
 
 export default function log(req: Request, res: Response, next: NextFunction) {
   const user = req.user
+  console.log(req.method)
   req.log = {
     user_id: user?.id,
     user_name: user?.name,
     request_url: req.originalUrl,
     request_ip: req.ip,
-    request_methods: req.method,
+    request_method: req.method,
     request_start_at: Date.now(),
   }
   next()
